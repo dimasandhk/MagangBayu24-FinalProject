19aaeaa27c7c2f2508a3d5b8b0d978e419eeb438
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2024-02-24 11:21:05 +0700

    feat: show board with opencv

--- vision/vision/gui_game.py
+++ vision/vision/gui_game.py
@@ -1,22 +1,57 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
+import cv2
 
-class MySubscriber(Node):
+# tilecount: 9 = (645, 766)
+# tilecount: 8 = (377, 766)
+# tilecount: 7 = (95, 766)
+# tilecount: 6 = (645, 497)
+# tilecount: 5 = (377, 497)
+# tilecount: 4 = (95, 497)
+# tilecount: 3 = (645, 215)
+# tilecount: 2 = (377, 215)
+# tilecount: 1 = (95, 215)
+
+class GUINode(Node):
+    pos_x = [95, 377, 645, 95, 377, 645, 95, 377, 645]
+    pos_y = [215, 215, 215, 497, 497, 497, 766, 766, 766]
+
     def __init__(self):
         super().__init__('my_subscriber')
         self.computer_subscriber = self.create_subscription(Int32, 'computer_move_topic', self.computer_callback, 10)
         self.player_subscriber = self.create_subscription(Int32, 'player_move_topic', self.player_callback, 10)
+        image_path = 'src/MagangBayu24-FinalProject/vision/resource/board.png'
+        print(image_path)
+        self.image = cv2.imread(image_path)
+        # cv2.namedWindow("Tictactoe Board", cv2.WINDOW_NORMAL)  # Create a named window
 
     def computer_callback(self, msg):
         self.get_logger().info('Received computer move: %d' % msg.data)
+        self.put_text(msg.data, 'c')
 
     def player_callback(self, msg):
         self.get_logger().info('Received player move: %d' % msg.data)
+        self.put_text(msg.data, 'p')
+
+    def put_text(self, tilecount, move):
+        tile_info = ['X', (0, 0, 255)] if move == 'p' else ['O', (255, 0, 0)]
+        cv2.putText(
+            self.image, 
+            tile_info[0], 
+            (self.pos_x[tilecount - 1], self.pos_y[tilecount - 1]), 
+            cv2.FONT_HERSHEY_SIMPLEX, 
+            5, 
+            tile_info[1], 
+            10
+        )
+        # Display the image with text
+        cv2.imshow("Tictactoe", self.image)
+        cv2.waitKey(500)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = MySubscriber()
+    node = GUINode()
     rclpy.spin(node)
     rclpy.shutdown()
 
