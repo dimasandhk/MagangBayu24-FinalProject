069387aea1ec54d12acf4187aa9c353e18ecbb36
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2024-02-24 12:41:38 +0700

    feat: prevent input when game over

--- vision/vision/gui_game.py
+++ vision/vision/gui_game.py
@@ -16,6 +16,8 @@ import cv2
 class GUINode(Node):
     pos_x = [95, 377, 645, 95, 377, 645, 95, 377, 645]
     pos_y = [215, 215, 215, 497, 497, 497, 766, 766, 766]
+    comp_moves = []
+    player_moves = []
 
     def __init__(self):
         super().__init__('my_subscriber')
@@ -24,15 +26,21 @@ class GUINode(Node):
         image_path = 'src/MagangBayu24-FinalProject/vision/resource/board.png'
         print(image_path)
         self.image = cv2.imread(image_path)
-        # cv2.namedWindow("Tictactoe Board", cv2.WINDOW_NORMAL)  # Create a named window
+        self.game_over = False
 
     def computer_callback(self, msg):
-        self.get_logger().info('Received computer move: %d' % msg.data)
-        self.put_text(msg.data, 'c')
+        if not self.game_over:
+            self.comp_moves.append(msg.data)
+            self.get_logger().info('Received computer move: %d' % msg.data)
+            self.put_text(msg.data, 'c')
+            self.check_game()
 
     def player_callback(self, msg):
-        self.get_logger().info('Received player move: %d' % msg.data)
-        self.put_text(msg.data, 'p')
+        if not self.game_over:
+            self.player_moves.append(msg.data)
+            self.get_logger().info('Received player move: %d' % msg.data)
+            self.put_text(msg.data, 'p')
+            self.check_game()
 
     def put_text(self, tilecount, move):
         tile_info = ['X', (0, 0, 255)] if move == 'p' else ['O', (255, 0, 0)]
@@ -48,6 +56,25 @@ class GUINode(Node):
         # Display the image with text
         cv2.imshow("Tictactoe", self.image)
         cv2.waitKey(500)
+    
+    def check_game(self):
+        if len(self.player_moves) + len(self.comp_moves) >= 9:
+            self.game_over = True
+            return
+
+        winning_combinations = [
+            [1, 2, 3], [4, 5, 6], [7, 8, 9],
+            [1, 4, 7], [2, 5, 8], [3, 6, 9],
+            [1, 5, 9], [3, 5, 7]
+        ]
+
+        for combo in winning_combinations:
+            if all(move in self.comp_moves for move in combo) or all(move in self.player_moves for move in combo):
+                self.game_over = True
+                return
+
+        self.game_over = False
+
 
 def main(args=None):
     rclpy.init(args=args)
